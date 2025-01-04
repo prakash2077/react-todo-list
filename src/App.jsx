149b1118bc301e318b085d7300745a984ac289c3
Rayudu Siva Sai Prakash <138751377+prakash2077@users.noreply.github.com>
2025-01-04 06:58:25 +0530

    Tried other way

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,28 @@ import ContactUs from "./components/ContactUs";
 
 const App = () => {
 
-  useEffect(() => {
-    const handleBeforeInstallPrompt = (event) => {
-      console.log('beforeinstallprompt event triggered');
-      // Let the browser display the native "Install App" prompt
-      event.prompt(); // This triggers the browser's "Install App" dialog
-      event.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the PWA installation');
-        } else {
-          console.log('User dismissed the PWA installation');
-        }
-      });
-    };
+  const addApp = () =>{
+  const handleBeforeInstallPrompt = (event) => {
+    console.log('beforeinstallprompt event triggered');
+    // Let the browser display the native "Install App" prompt
+    event.prompt(); // This triggers the browser's "Install App" dialog
+    event.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the PWA installation');
+      } else {
+        console.log('User dismissed the PWA installation');
+      }
+    });
+  };
 
-    // Listen for the beforeinstallprompt event
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+  // Listen for the beforeinstallprompt event
+  window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
 
-    return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    };
-  }, []);
+  return () => {
+    window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+  };
+  }
+  addApp();
 
   return (
     <BrowserRouter>
